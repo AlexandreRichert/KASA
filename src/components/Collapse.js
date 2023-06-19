1acf7a25d39b3fe5d98bb3b39d8cc2e4f2834143
Alexandre Richert <121611457+AlexandreRichert@users.noreply.github.com>
2023-06-19 23:10:46 +0200

    ajout des chevrons up sur les collapses. Ajout condition si une seule photo dans caroussel --> pas de fleches

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -11,21 +11,24 @@ function Collapse (props) {
     if (useListItem) {
       return (
         <li className={`collapse ${active && "active"}`}>
-          <h2 onClick={handleToggle}>{props.title}</h2>
+          <h2>{props.title}</h2>
+          <i className={`fa-solid fa-chevron-up ${active && "rotate"}`} onClick={handleToggle}></i>
           <p className="collapse-content">{props.description}</p>
         </li>
       );
     } else if (isDescription){
       return (
         <div className={` description collapse ${active && "active"}`}>
-          <h2 onClick={handleToggle}>{props.title}</h2>
+          <h2>{props.title}</h2>
+          <i className="fa-solid fa-chevron-up" onClick={handleToggle}></i>
           <p className="collapse-content">{props.description}</p>
         </div>
       );
     } else {
         return (
           <div className={` equipments collapse ${active && "active"}`}>
-            <h2 onClick={handleToggle}>{props.title}</h2>
+            <h2>{props.title}</h2>
+            <i className="fa-solid fa-chevron-up" onClick={handleToggle}></i>
             <p className="collapse-content">{props.equipements}</p>
           </div>
         );
--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,31 +1,34 @@
 import React, { useState } from 'react'
 
 export default function Slideshow(props) {
-
+// Initialisation de l'useState pour changer d'image
 const [currentIndex, setCurrentIndex] = useState(0);
 
+// Tableau des images du logement
 const pictures = props.pictures.map((photo) => (
     <img key={photo} src={photo} alt="" />
   )); 
 
+//Fonction pour afficher l'image suivante
 const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % pictures.length);
 };
 
+// Fonction pour afficher l'image précédente
 const previousImage = () => {
 setCurrentIndex((prevIndex) => (prevIndex - 1 + pictures.length) % pictures.length);
 };
 
 return (
     <div className='slideshow'>
-    {props.pictures.length > 0 ? (
+    {props.pictures.length > 1 ? (
             <>
               <img src={pictures[currentIndex].props.src} alt={` ${currentIndex}`} />
               <i className="fa-solid fa-chevron-left" onClick={previousImage}></i>
               <i className="fa-solid fa-chevron-right" onClick={nextImage}></i>
             </>
           ) : (
-            <p>Aucune image disponible.</p>
+            <img src={pictures[currentIndex].props.src} alt={` ${currentIndex}`} />
           )}      
     </div>
 
